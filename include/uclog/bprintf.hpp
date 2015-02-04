c2ed1b43dedc1f1224380f44e24d1926cbfefd88
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2015-02-04 23:49:56 +0100

    added piece of bprintf documentation

--- include/uclog/bprintf.hpp
+++ include/uclog/bprintf.hpp
@@ -8,6 +8,14 @@
 namespace uclog
 {
 
+/*
+ * Conversion specifiers %f %e %g %a are serialized as floats, %lf %le %lg %la as doubles.
+ *
+ * Not implemented:
+ * - %n conversion specifier
+ * - long double formats
+ */
+
 size_t snbprintf(uint8_t* buf, size_t size, const char* fmt, ...);
 
 size_t vsnbprintf(uint8_t* buf, size_t size, const char* fmt, va_list args);
