bcf8d74055b28c104bc6c2ef1b371968410bd6d0
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2015-02-06 02:56:54 +0100

    function objects used in test (more flexibility)

--- test/perf/perf.cpp
+++ test/perf/perf.cpp
@@ -35,7 +35,29 @@ struct test_result
     double v2;
 };
 
-template <typename TestCase>
+struct printf_adapter
+{
+    void operator()(void* buf, size_t size, const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        vsnprintf(static_cast<char*>(buf), size, fmt, args);
+        va_end(args);
+    }
+};
+
+struct bprintf_adapter
+{
+    void operator()(void* buf, size_t size, const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        uclog::vsnbprintf(buf, size, fmt, args);
+        va_end(args);
+    }
+};
+
+template <template <typename> class TestCase>
 test_result test(const char* name)
 {
     test_result res;
@@ -44,68 +66,68 @@ test_result test(const char* name)
         measure_time _(&res.v1);
         for (int i = 0; i < N; i++)
         {
-            TestCase()(&snprintf);
+            TestCase<printf_adapter>()();
         }
     }
     {
         measure_time _(&res.v2);
         for (int i = 0; i < N; i++)
         {
-            TestCase()(&uclog::snbprintf);
+            TestCase<bprintf_adapter>()();
         }
     }
     return res;
 }
 
+template <typename FuncType>
 struct test_fmt
 {
-    template <typename FuncType>
-    void operator()(FuncType fun)
+    void operator()()
     {
         char buf[1024];
-        fun(buf, 1024,
+        FuncType()(buf, 1024,
             "hisuhdspoaiuhdaouhfaodshapsdgoihsdgouashpfdhfp[wenpimc[ejmiejrcqwejrwepijraoakpsoka"
         );
     }
 };
 
+template <typename FuncType>
 struct test_strings
 {
-    template <typename FuncType>
-    void operator()(FuncType fun)
+    void operator()()
     {
         char buf[1024];
-        fun(buf, 1024, "%s %s %s", "ala", "ma", "kota");
+        FuncType()(buf, 1024, "%s %s %s", "ala", "ma", "kota");
     }
 };
 
+template <typename FuncType>
 struct test_int
 {
-    template <typename FuncType>
-    void operator()(FuncType fun)
+    void operator()()
     {
         char buf[1024];
-        fun(buf, 1024, "%d %d %d %d", 42, 42, 42, 42);
+        FuncType()(buf, 1024, "%d %d %d %d", 42, 42, 42, 42);
     }
 };
 
+template <typename FuncType>
 struct test_float
 {
-    template <typename FuncType>
-    void operator()(FuncType fun)
+    void operator()()
     {
         char buf[1024];
-        fun(buf, 1024, "%f %le %G %lA", 42., 42., 42., 42.);
+        FuncType()(buf, 1024, "%f %le %G %lA", 42., 42., 42., 42.);
     }
 };
 
+template <typename FuncType>
 struct test_many_ints
 {
-    template <typename FuncType>
-    void operator()(FuncType fun)
+    void operator()()
     {
         char buf[1024];
-        fun(buf, 1024,
+        FuncType()(buf, 1024,
             "%hhd %hhd %hhd %hhd "
             "%hd %hd %hd %hd "
             "%d %d %d %d "
