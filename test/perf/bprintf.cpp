ce191a3f8440bf5faae1d2993106b0568bbab2d5
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2016-06-26 22:03:08 +0200

    bprintf performance test finished with args_encode

--- test/perf/bprintf.cpp
+++ test/perf/bprintf.cpp
@@ -15,11 +15,14 @@ struct test_result
     double v1;
     double v2;
     double v3;
+    double v4;
 };
+
+typedef std::vector<arg_type> argtypes_t;
 
 struct printf_adapter
 {
-    void operator()(void* buf, size_t size, size_t narg, const char* fmt, ...)
+    void operator()(void* buf, size_t size, const argtypes_t& types, const char* fmt, ...)
     {
         va_list args;
         va_start(args, fmt);
@@ -30,7 +33,7 @@ struct printf_adapter
 
 struct bprintf_adapter
 {
-    void operator()(void* buf, size_t size, size_t narg, const char* fmt, ...)
+    void operator()(void* buf, size_t size, const argtypes_t& types, const char* fmt, ...)
     {
         va_list args;
         va_start(args, fmt);
@@ -44,14 +47,25 @@ struct site_adapter
     static arg_type args[32];
     static logger lgr;
 
-    void operator()(void* buf, size_t size, size_t narg, const char* fmt, ...)
+    void operator()(void* buf, size_t size, const argtypes_t& types, const char* fmt, ...)
     {
-        site_t(lgr, level_critical, fmt, array_view<arg_type>(args, narg));
+        site_t(lgr, level_critical, fmt, array_view<arg_type>(args, types.size()));
     }
 };
 
 arg_type site_adapter::args[32];
 logger site_adapter::lgr;
+
+struct argsencode_adapter
+{
+    void operator()(void* buf, size_t size, const argtypes_t& types, const char* fmt, ...)
+    {
+        va_list args;
+        va_start(args, fmt);
+        vargs_encode(buf, size, types.data(), types.size(), args);
+        va_end(args);
+    }
+};
 
 template <template <typename> class TestCase>
 test_result test(const char* name)
@@ -79,58 +93,87 @@ test_result test(const char* name)
             TestCase<site_adapter>()();
         }
     }
+    {
+        measure_time _(&res.v4);
+        for (int i = 0; i < N; i++)
+        {
+            TestCase<argsencode_adapter>()();
+        }
+    }
     return res;
 }
 
 template <typename FuncType>
 struct test_fmt
 {
+    static const argtypes_t types;
+
     void operator()()
     {
         char buf[1024];
-        FuncType()(buf, 1024, 1,
+        FuncType()(buf, 1024, types,
             "hisuhdspoaiuhdaouhfaodshapsdgoihsdgouashpfdhfp[wenpimc[ejmiejrcqwejrwepijraoakpsoka"
         );
     }
 };
 
 template <typename FuncType>
+const argtypes_t test_fmt<FuncType>::types = {};
+
+template <typename FuncType>
 struct test_strings
 {
+    static const argtypes_t types;
+
     void operator()()
     {
         char buf[1024];
-        FuncType()(buf, 1024, 3, "%s %s %s", "ala", "ma", "kota");
+        FuncType()(buf, 1024, types, "%s %s %s", "ala", "ma", "kota");
     }
 };
+
+template <typename FuncType>
+const argtypes_t test_strings<FuncType>::types = {arg_type_string, arg_type_string, arg_type_string};
 
 template <typename FuncType>
 struct test_int
 {
+    static const argtypes_t types;
+
     void operator()()
     {
         char buf[1024];
-        FuncType()(buf, 1024, 4, "%d %d %d %d", 42, 42, 42, 42);
+        FuncType()(buf, 1024, types, "%d %d %d %d", 42, 42, 42, 42);
     }
 };
+
+template <typename FuncType>
+const argtypes_t test_int<FuncType>::types = {arg_type_int, arg_type_int, arg_type_int, arg_type_int};
 
 template <typename FuncType>
 struct test_float
 {
+    static const argtypes_t types;
+
     void operator()()
     {
         char buf[1024];
-        FuncType()(buf, 1024, 4, "%f %le %G %lA", 42., 42., 42., 42.);
+        FuncType()(buf, 1024, types, "%f %le %G %lA", 42., 42., 42., 42.);
     }
 };
 
 template <typename FuncType>
+const argtypes_t test_float<FuncType>::types = {arg_type_float, arg_type_double, arg_type_float, arg_type_double};
+
+template <typename FuncType>
 struct test_many_ints
 {
+    static const argtypes_t types;
+
     void operator()()
     {
         char buf[1024];
-        FuncType()(buf, 1024, 20,
+        FuncType()(buf, 1024, types,
             "%hhd %hhd %hhd %hhd "
             "%hd %hd %hd %hd "
             "%d %d %d %d "
@@ -145,6 +188,15 @@ struct test_many_ints
     }
 };
 
+template <typename FuncType>
+const argtypes_t test_many_ints<FuncType>::types = {
+    arg_type_char, arg_type_char, arg_type_char, arg_type_char,
+    arg_type_short, arg_type_short, arg_type_short, arg_type_short,
+    arg_type_int, arg_type_int, arg_type_int, arg_type_int,
+    arg_type_long, arg_type_long, arg_type_long, arg_type_long,
+    arg_type_long_long, arg_type_long_long, arg_type_long_long, arg_type_long_long
+};
+
 int main()
 {
     test_result results[] = {
@@ -155,10 +207,10 @@ int main()
         test<test_many_ints>("test_many_ints")
     };
 
-    printf("%20s %20s %20s %20s\n", "", "snprintf", "snbprintf", "site");
+    printf("%20s %20s %20s %20s %20s\n", "", "snprintf", "snbprintf", "site", "args_encode");
     for (auto& res : results)
     {
-        printf("%-20s %20lf %20lf %20lf\n", res.name, res.v1, res.v2, res.v3);
+        printf("%-20s %20lf %20lf %20lf %20lf\n", res.name, res.v1, res.v2, res.v3, res.v4);
     }
 
     return 0;
