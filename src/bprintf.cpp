7fb27d284b99b214ec0a1500eb4434f8677e1bb1
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2016-06-26 21:26:26 +0200

    copy-pastes in bprintf reduced (but not eliminated: switch)

--- src/bprintf.cpp
+++ src/bprintf.cpp
@@ -140,25 +140,20 @@ size_t arg_decode(const char* fmt, arg_spec* spec)
     }
 }
 
-size_t args_encode(void* buf, size_t size, const arg_type* types, size_t ntypes, ...)
-{
-    va_list args;
-    va_start(args, ntypes);
-    size_t written = vargs_encode(buf, size, types, ntypes, args);
-    va_end(args);
-    return written;
-}
-
-size_t vargs_encode(void* buf, size_t size, const arg_type* types, size_t ntypes, va_list args)
-{
+/// due to "default argument promotions" as described in 6.5.2.2/6,
+/// char and short arguments are promoted to int
 #define pull_int_arg(type)                                                     \
     ((sizeof(type) == sizeof(unsigned long long)) ?                            \
         static_cast<type>(va_arg(args, unsigned long long)) :                  \
         static_cast<type>(va_arg(args, int)))
 
+/// due to "default argument promotions" as described in 6.5.2.2/6,
+/// float is promoted to double
 #define pull_float_arg(type)                                                   \
     (static_cast<type>(va_arg(args, double)))
 
+size_t vargs_encode(void* buf, size_t size, const arg_type* types, size_t ntypes, va_list args)
+{
     stream str(static_cast<uint8_t*>(buf), static_cast<uint8_t*>(buf) + size);
 
     for (size_t i = 0; i < ntypes; ++i)
@@ -198,56 +193,16 @@ size_t vargs_encode(void* buf, size_t size, const arg_type* types, size_t ntypes
     }
 
     return str.written();
-
-#undef pull_int_arg
-#undef pull_float_arg
 }
 
 size_t vsnbprintf(void* buf, size_t size, const char* fmt, va_list args)
 {
-    /// due to "default argument promotions" as described in 6.5.2.2/6,
-    /// char and short arguments are promoted to int
-#define save_arg(type)                                  \
-do                                                      \
-{                                                       \
-    type value;                                         \
-    if (sizeof(type) == sizeof(unsigned long long))     \
-    {                                                   \
-        value = va_arg(args, unsigned long long);       \
-    }                                                   \
-    else                                                \
-    {                                                   \
-        value = va_arg(args, int);                      \
-    }                                                   \
-    if (cur + sizeof(type) <= end)                      \
-    {                                                   \
-        memcpy(cur, &value, sizeof(type));              \
-        cur += sizeof(type);                            \
-    }                                                   \
-} while (0)
-
-    /// due to "default argument promotions" as described in 6.5.2.2/6,
-    /// float is promoted to double
-#define save_float_arg(type)                            \
-do                                                      \
-{                                                       \
-    type value = va_arg(args, double);                  \
-    if (cur + sizeof(type) <= end)                      \
-    {                                                   \
-        memcpy(cur, &value, sizeof(type));              \
-        cur += sizeof(type);                            \
-    }                                                   \
-} while (0)
-
-    arg_spec spec = { arg_type_none };
-    const uint8_t* start = static_cast<const uint8_t*>(buf);
-    const uint8_t* end = static_cast<const uint8_t*>(buf) + size;
-    uint8_t* cur = static_cast<uint8_t*>(buf);
+    stream str(static_cast<uint8_t*>(buf), static_cast<uint8_t*>(buf) + size);
 
     while (*fmt)
     {
+        arg_spec spec;
         int read = arg_decode(fmt, &spec);
-
         fmt += read;
 
         switch (spec.type)
@@ -255,46 +210,45 @@ do                                                      \
         case arg_type_none:
             break;
         case arg_type_char:
-            save_arg(char);
+            str.put(pull_int_arg(char));
             break;
         case arg_type_short:
-            save_arg(short);
+            str.put(pull_int_arg(short));
             break;
         case arg_type_int:
-            save_arg(int);
+            str.put(pull_int_arg(int));
             break;
         case arg_type_long:
-            save_arg(long);
+            str.put(pull_int_arg(long));
             break;
         case arg_type_long_long:
-            save_arg(long long);
+            str.put(pull_int_arg(long long));
             break;
         case arg_type_float:
-            save_float_arg(float);
+            str.put(pull_float_arg(float));
             break;
         case arg_type_double:
-            save_float_arg(double);
+            str.put(pull_float_arg(double));
             break;
         case arg_type_pointer:
-            save_arg(uintptr_t);
+            str.put(pull_int_arg(uintptr_t));
             break;
         case arg_type_string:
-            {
-                const char* str = va_arg(args, const char*);
-                size_t len = strlen(str) + 1;
-                if (cur + len <= end)
-                {
-                    memcpy(cur, str, len);
-                    cur += len;
-                }
-                break;
-            }
+            str.put(va_arg(args, const char*));
+            break;
         }
     }
 
-    return cur - start;
-#undef save_arg
-#undef save_float_arg
+    return str.written();
+}
+
+size_t args_encode(void* buf, size_t size, const arg_type* types, size_t ntypes, ...)
+{
+    va_list args;
+    va_start(args, ntypes);
+    size_t written = vargs_encode(buf, size, types, ntypes, args);
+    va_end(args);
+    return written;
 }
 
 size_t snbprintf(void* buf, size_t size, const char* fmt, ...)
